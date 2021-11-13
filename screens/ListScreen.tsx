480b8a8aa970985b027866736872df4d6679b68c
Nithaparan Francis <nithaparanf@gmail.com>
2021-11-12 23:24:44 -0500

    added search bar in lists screen

--- screens/ListScreen.tsx
+++ screens/ListScreen.tsx
@@ -1,7 +1,8 @@
 import { NavigationRouteContext } from '@react-navigation/core';
 import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, TouchableOpacity } from 'react-native';
-
+import { SafeAreaView, View, FlatList, StyleSheet, Text,Image,TextInput, StatusBar, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { Feather } from "@expo/vector-icons";
 import ListItem from '../compornents/ListItem';
 
 const DATA = [
@@ -40,6 +41,24 @@ const ListScreen = ({navigation}: {navigation: any}) => {
           navigation.navigate('Detail');
           }}>
         
+        <View style={styles.topBarContainerStyle}>
+        <View style={styles.searchBarContainerStyle}>
+        <Image
+            style={styles.searchIconStyle}
+            source={require("../assets/search.png")}
+          />
+        <TextInput style={styles.searchBarStyle} placeholder="Search for a product"/>
+        </View>
+        
+        <View style={styles.filterButtonContainer}>
+        <Image
+            style={styles.filterButtonStyle}
+            source={require("../assets/filter.png")}
+          />
+        </View>
+        </View>
+        
+        
         <FlatList
           data={DATA}
           renderItem={ListItem}
@@ -65,6 +84,53 @@ const ListScreen = ({navigation}: {navigation: any}) => {
     },
     title: {
       fontSize: 32,
+    },
+    topBarContainerStyle: {
+      alignItems: "center",
+      flexDirection: "row",
+      flexWrap: "wrap",
+    },
+    searchBarContainerStyle: {
+      flex: 1,
+      height: 50,
+      borderRadius: 15,
+      marginRight: 20,
+      marginLeft: 20,
+      marginTop: 20,
+      paddingLeft: 20,
+      backgroundColor: '#f8f8f8',
+      
+      flexDirection: "row",
+      flexWrap: "wrap",
+      alignItems: "center",
+      alignContent: 'center',
+    },
+    filterButtonContainer: {
+      height: 50,
+      width: 50,
+      marginRight: 20,
+      backgroundColor: '#f8f8f8',
+      alignContent: 'center',
+      borderRadius: 15,
+      alignItems: "center",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: 'center',
+    },
+    filterButtonStyle: {
+      width: '80%',
+    height: undefined,
+    aspectRatio: 1,
+    },
+    searchBarStyle: {
+      flex: 3,
+      marginLeft: 20,
+    },
+    searchIconStyle: {
+      height: 22,
+      width: undefined,
+      aspectRatio: 1,
+      alignItems: "flex-start",
     },
   });
   
