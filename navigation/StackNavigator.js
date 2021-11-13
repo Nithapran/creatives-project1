068f7510c1f7d00240ed8954b9204ee358a78cee
tomcha34 <70660830+tomcha34@users.noreply.github.com>
2021-11-12 19:42:35 -0500

    Deleted useNotification Import
    Deleted Use Notification Import

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,25 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
-
-import MainScreen from '../screens/MainScreen';
-import FavouriteScreen from '../screens/FavouriteScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-import ListScreen from '../screens/ListScreen';
-import DetailScreen from '../screens/DetailScreen';
-
+import MainScreen from "../screens/MainScreen";
+import FavouriteScreen from "../screens/FavouriteScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import ListScreen from "../screens/ListScreen";
+import DetailScreen from "../screens/DetailScreen";
 
 const Stack = createNativeStackNavigator();
 
 function StackNavigator() {
   return (
-    
-      <Stack.Navigator>
-        <Stack.Screen name="Main" component={MainScreen}  />
-        <Stack.Screen name="Lists" component={ListScreen} /> 
-        <Stack.Screen name="Detail" component={DetailScreen} />
-      </Stack.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen name="Main" component={MainScreen} />
+      <Stack.Screen name="Lists" component={ListScreen} />
+      <Stack.Screen name="Detail" component={DetailScreen} />
+    </Stack.Navigator>
   );
 }
 
